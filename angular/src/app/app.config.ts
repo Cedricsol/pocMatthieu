3327f6a06f68f154b6aa9c775185facd8107f36d
Cédric <cedric.gautier@consultants-solutec.fr>
2026-03-23 11:36:58 +0100

    setup keycloak for angular

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideKeycloak } from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8082',
+        realm: 'test',
+        clientId: 'angular',
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
+        pkceMethod: 'S256',
+      },
+    }),
+  ],
 };
--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
