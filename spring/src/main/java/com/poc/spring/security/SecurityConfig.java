5d6b2876b731395df2b380d46ca095628e9b35d4
Cédric <cedric.gautier@consultants-solutec.fr>
2026-03-24 17:11:54 +0100

    clean spring

--- spring/src/main/java/com/poc/spring/security/SecurityConfig.java
+++ spring/src/main/java/com/poc/spring/security/SecurityConfig.java
@@ -2,16 +2,12 @@ package com.poc.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,15 +19,9 @@ public class SecurityConfig {
         http
         .cors(cors -> {})
         .authorizeHttpRequests(auth -> auth
-            .antMatchers("/api/**").authenticated()
             .anyRequest().authenticated()
         )
         .oauth2Login(Customizer.withDefaults())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-        .exceptionHandling(ex -> ex.defaultAuthenticationEntryPointFor(
-            new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), 
-            new AntPathRequestMatcher("/api/**"))
-        )
         .csrf(csrf -> csrf.disable())
         .logout(logout -> logout.logoutSuccessHandler(logoutSuccessHandler)
         .invalidateHttpSession(true)
